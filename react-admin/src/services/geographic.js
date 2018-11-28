1bb2596214a090920d96521dd1c714d56d395deb
zhooson <861077046@qq.com>
2018-11-28 11:01:26 +0800

    fix react-admin

---
+++ react-admin/src/services/geographic.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request';
+
+export async function queryProvince() {
+    return request('/geographic/province');
+}
+
+export async function queryCity(province) {
+    return request(`/geographic/city/${province}`);
+}
